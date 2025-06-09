421efeeebe129ecb87980d6caedf8c08e9bc5bc3
foxxdc <mayomlaejanpong@gmail.com>
2025-06-09 21:47:47 +0700

    Update Exercise4_Sawakrit_L.py

--- Exercise4_Sawakrit_L.py
+++ Exercise4_Sawakrit_L.py
@@ -1,8 +1,16 @@
-Python 3.13.4 (v3.13.4:8a526ec7cbe, Jun  3 2025, 21:14:54) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
->>> A = Name = "Sawakrit Laejanpong"
->>> B = Age = 17
->>> X = Height = "5'8 Ft"
->>> Y = Weight = "56 kg"
->>> print(Name, Age, Height, Weight)
-Sawakrit Laejanpong 17 5'8 Ft 56 kg
+print ()
+print("Tell me YOUR SCORE in this semester")
+FE = float(input("FE(25Point) : "))
+GB = float(input("GB(25Point) : "))
+ICS = float(input("ICS(25Point) : "))
+CP = float(input("CP(25Point) : "))
+
+print()
+print("--- Your Total Score ---")
+print (FE, "Points")
+print (GB, "Points")
+print (ICS, "Points")
+print (CP, "Points")
+
+print()
+print("--- Your Total Score is!",FE+GB+ICS+CP,"/100 Points---")
