3970a59249bc4735497b237441b0ade41de053ba
foxxdc <mayomlaejanpong@gmail.com>
2025-06-09 21:17:21 +0700

    Add files via upload

---
+++ Exercise4_Sawakrit_L.py
@@ -0,0 +1,8 @@
+Python 3.13.4 (v3.13.4:8a526ec7cbe, Jun  3 2025, 21:14:54) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> A = Name = "Sawakrit Laejanpong"
+>>> B = Age = 17
+>>> X = Height = "5'8 Ft"
+>>> Y = Weight = "56 kg"
+>>> print(Name, Age, Height, Weight)
+Sawakrit Laejanpong 17 5'8 Ft 56 kg
