7d0a563880ea53de5983277e1992c4a482ee6e2d
foxxdc <mayomlaejanpong@gmail.com>
2025-06-11 00:57:20 +0700

    Create Exercise5_1_Sawakrit_L.py

---
+++ Exercise5_1_Sawakrit_L.py
@@ -0,0 +1,14 @@
+print("-----Welcome----")
+Num1=int(input("Please Insert Your First Number"))
+Num2=int(input("Please Insert Your Second Number"))
+plus= Num1 + Num2
+minus= Num1 - Num2
+multi = Num1 * Num2
+divine = Num1/Num2
+print("-------Calulated-------")
+print(Num1,"+",Num2,"=",plus)
+print(Num1,"-",Num2,"=",minus)
+print(Num1,"*",Num2,"=",multi)
+print(Num1,"/",Num2,"=",divine)
+
+print("--ended---")
