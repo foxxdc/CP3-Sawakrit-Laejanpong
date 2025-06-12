fda7a1b62171646b838a8fa25f742bdd4cd429ac
foxxdc <mayomlaejanpong@gmail.com>
2025-06-12 22:22:16 +0700

    Create Exercise8_Sawakrit_L.py

---
+++ Exercise8_Sawakrit_L.py
@@ -0,0 +1,34 @@
+usernameInput = input("Username: ")
+passwordInput = input("Password: ")
+if usernameInput == "silv" and passwordInput == "1150" :
+  print("Login Successfully!")
+  print("---Welcome!---")
+  print("---CS 1.6 ahh Shop---")
+  print("1. AWP - 4700$")
+  print("2. AK47 - 2700$")
+  print("3. Galil - 1800$")
+  userPick = int(input(">> "))
+  if userPick == 1:
+    print("That will cost 4700$")
+    print("How many you wanna buy?")
+    price1 = 4700
+    amount1 = int(input(":"))
+    print(price1 * amount1)
+    print("Purchased!")
+    print("GO CRAZY LIKE S1MPLE!")
+  elif userPick == 2:
+    print("That will cost 2700$")
+    print("How many you wanna buy?")
+    price2 = 2700
+    amount2 = int(input(":"))
+    print(price2 * amount2)
+    print("Purchased!")
+    print("GO CRAZY LIKE DONK!")
+  elif userPick3 == 3:
+    print("That will cost 1800$")
+    print("How many you wanna buy?")
+    price3 = 1800
+    amount3 = int(input(":"))
+    print(price3 * amount3)
+    print("Purchased!")
+    print("GO CRAZY LIKE-... idk like who lol")    
