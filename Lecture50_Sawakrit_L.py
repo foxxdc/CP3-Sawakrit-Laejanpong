6fb7183d6add074f2172765a720f499bc707d666
foxxdc <mayomlaejanpong@gmail.com>
2025-06-18 21:32:55 +0700

    Create Lecture50_Sawakrit_L.py

---
+++ Lecture50_Sawakrit_L.py
@@ -0,0 +1,12 @@
+fn = int(input("First Number! : "))
+sn = int(input("Second Number!: "))
+
+def addNumber(x,y):
+    print(fn+sn)
+def calculateNumber(x,y):
+    print(fn-sn)
+    print(fn*sn)
+    print(fn/sn)
+
+addNumber(fn,sn)
+calculateNumber(fn,sn)
